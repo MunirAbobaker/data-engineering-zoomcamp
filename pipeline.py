fa648557cb4a9504ecb9686b4dbeedd724a295d4
MunirAbobaker <46633767+MunirAbobaker@users.noreply.github.com>
2022-07-09 11:49:03 +0200

    data-enginnering:- connect postgres to notebook

---
+++ pipeline.py
@@ -0,0 +1,12 @@
+import sys
+
+import pandas
+
+
+print(sys.argv)
+
+day = sys.argv[1] # 0 is the name of the file, and 1 is the arg. we give to bash
+
+
+
+print(f'Job finished successfully for day = {day}')
